fbaad1fb9b9a04aeedd96c8186ff9e7393b74211
rgnt <maros.prejsa1@gmail.com>
2020-09-26 17:38:25 +0200

    UTF8 meta

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,6 +1,20 @@
+let count = 0;
+
 $( document ).ready(function() {
   	var clipboard = new ClipboardJS('.discord-id-copy');
+
+
+	$("#header-banner-name").click(function() {
+		count++;
+		if(count >= 10) {
+			$("body").css({"background": "url('sources/img/noise.png'), #634242", "background-blend-mode": "luminosity"});
+
+			$("#header-banner-name").html("wener");
+			$("#header-banner-aliases").html("(Le Wenęůr, MrWener)");
+		}
+	});
 });
+
 
 function showContact() {
 	$('#contact').css({'display': 'block','animation-name': 'pulse'});
